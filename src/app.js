ad42d6784cd2f15cc0729d34a6509c656503c565
Marta <mmlingdata@gmail.com>
2021-08-29 10:33:20 +0200

    Added the second api

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
@@ -29,7 +29,7 @@ function displayForecast() {
     forecastHTML =
       forecastHTML +
       `              <div class="col-2">
-                <div class="forecast-day">Sun</div>
+                <div class="forecast-day">${day}</div>
                 <img
                   src="http://openweathermap.org/img/wn/10d@2x.png"
                   alt=""
@@ -46,6 +46,12 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "69a00854f19596cde60a5c7f8d9fecf6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -70,6 +76,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -115,4 +122,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Poznań");
-displayForecast();
