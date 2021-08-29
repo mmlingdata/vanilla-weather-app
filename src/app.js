26e0255920da084fb233a9c68f0307ee122d098e
Marta <mmlingdata@gmail.com>
2021-08-29 11:16:39 +0200

    Added units metric

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "69a00854f19596cde60a5c7f8d9fecf6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
